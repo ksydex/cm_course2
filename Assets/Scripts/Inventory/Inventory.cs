d5c4fefa4db47f537230886f4929b2b8e0f463c8
Artyom Lukyanov <artglz63@gmail.com>
2022-01-24 02:01:34 +0300

    initial

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public bool[] isFull;
+    public GameObject[] slots;
+    public GameObject inventory;
+    private bool inventoryOn;
+
+    private void Start()
+    {
+        inventoryOn = false;
+    }
+
+    public void Chest()
+    {
+        if (inventoryOn == false)
+        {
+            inventoryOn = true;
+            inventory.SetActive(true);
+        }
+        else if (inventoryOn == true)
+        {
+            inventoryOn = false;
+            inventory.SetActive(false);
+        }
+    }
+}
